f3f01914435d0cc571fca04eb2ad5eff6e999ce5
David <davidferreira.m1@outlook.com>
2020-11-06 09:51:05 -0500

    feat(stylesheet): arquivo de cores criado

---
+++ src/stylesheet/default-theme/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  'brand-primary': {
+    main: '#B05800',
+    contrast: '#ffffff',
+  },
+  'brand-secondary': {
+    main: '#D18A43',
+    contrast: '#ffffff',
+  },
+  'action-danger': {
+    main: '#C01A24',
+    contrast: '#ffffff',
+  },
+  'action-confirm': {
+    main: '#0066B2',
+    contrast: '#ffffff',
+  },
+  'action-error': {
+    main: '#B21A20',
+    contrast: '#ffffff',
+  },
+};
