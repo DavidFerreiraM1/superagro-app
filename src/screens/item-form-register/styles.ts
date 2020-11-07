2245410e93cc103ddcf057637dfce138866b18fa
David <davidferreira.m1@outlook.com>
2020-11-07 18:41:12 -0300

    feat(form register): header para tela de registro criada

---
+++ src/screens/item-form-register/styles.ts
@@ -0,0 +1,62 @@
+import Styled from 'styled-components/native';
+import {DefaultColors} from '../../design-tokens';
+
+export const ContentScreen = Styled.View`
+  flex: 1;
+  background-color: ${DefaultColors['brand-secondary'].main};
+`;
+
+export const HeaderRoot = Styled.View`
+  width: 100%;
+  height: 124px;
+  background-color: ${DefaultColors['brand-secondary'].main};
+`;
+
+export const HeaderContent = Styled.View`
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const BackButtonArea = Styled.View`
+  width: 48px
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BackButton = Styled.TouchableOpacity`
+  width: 100%
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TitleArea = Styled.View`
+  height: 100%;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TitleBox = Styled.View`
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 88px;
+  right: 24px
+`;
+
+export const TitleImage = Styled.Image`
+  height: 42px;
+  width: 45px;
+`;
+
+export const NewItemDescription = Styled.Text`
+  margin-vertical: 4px;
+  font-family: Ubuntu-Light;
+  font-size: 18px;
+  text-align: center;
+  color: ${DefaultColors['action-primary'].main};
+`;
