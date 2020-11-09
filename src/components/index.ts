038072b381e035633b2e3580061e80e5a15e12ba
David <davidferreira.m1@outlook.com>
2020-11-09 18:25:31 -0500

    feat(drawer): drawer criado

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {Button, BtnContentText, BtnSelect, BtnSelectText} from './button';
 export {TextInput, TextInputStyled} from './input';
 export {ContainerScreen} from './container-screen/container-screen';
+export {Drawer} from './drawer/drawer';
