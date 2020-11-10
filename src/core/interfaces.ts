fdcf799c5e4571b3b328f9734792095c04d66235
David <davidferreira.m1@outlook.com>
2020-11-10 15:54:13 -0500

    feat(redux): interface para dados de animais criada

---
+++ src/core/interfaces.ts
@@ -0,0 +1,13 @@
+export interface IAnimal {
+  tipoAnimal: string;
+  nome: string;
+  localizacao: string;
+  dataNascimento: string;
+  entradaPlantel: string;
+  pesoCompra: string;
+  raca: string;
+  codigoRastreamento: string;
+  faseProducao: string;
+  tipoGranja: string;
+  statusAnimal: string;
+}
