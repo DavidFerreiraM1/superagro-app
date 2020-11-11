aedcc6dd38b419282dae117342cf038e547481b9
David <davidferreira.m1@outlook.com>
2020-11-11 12:12:58 -0500

    feat(redux): valores estáticos ajustados

--- src/screens/animal-info/animal-info-page.tsx
+++ src/screens/animal-info/animal-info-page.tsx
@@ -1,5 +1,5 @@
-import {StackHeaderProps} from '@react-navigation/stack';
-import React, {useEffect} from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {ContainerScreen} from '../../components';
 import {
@@ -10,16 +10,52 @@ import {
   HeaderRoot,
 } from '../../components/dashboard';
 import {ContentScreen, FarmIconBox, FarmIconIMage} from './styles';
-import PoultryImage from '../../assets/images/poultry.png';
 import ArrowBack from '../../assets/icons/arrow.svg';
 import {DefaultColors} from '../../design-tokens';
+import {getAnimalOnRealm} from './animal-info-service';
+
+import PigImage from '../../assets/images/pig.png';
+import PoultryImage from '../../assets/images/poultry.png';
 
 const defaultColor = DefaultColors['brand-primary'].main;
+const headerIconImage: any = {
+  swine: {
+    text: 'Suíno',
+    url: PigImage,
+    style: {height: 29, width: 43},
+  },
+  poultry: {
+    text: 'Aves',
+    url: PoultryImage,
+    style: {height: 42, width: 45},
+  },
+};
 
-export function AnimalInfoPage(props: StackHeaderProps) {
+export function AnimalInfoPage(props: StackScreenProps<any>) {
+  const [data, setData] = useState({
+    tipoAnimal: 'swine',
+    nome: '',
+    localizacao: '',
+    dataNascimento: '',
+    entradaPlantel: '',
+    pesoCompra: '',
+    raca: '',
+    codigoRastreamento: '',
+    faseProducao: '',
+    tipoGranja: '',
+    statusAnimal: '',
+  });
+
   useEffect(() => {
-    // console.log(props.navigation.);
+    const getDataOnRealm = async () => {
+      setData(await getAnimalOnRealm(props.route.params?.animalId));
+    };
+    getDataOnRealm();
   }, []);
+
+  useEffect(() => {
+    console.log('Data Alterou', data);
+  }, [data]);
 
   return (
     <ContainerScreen>
@@ -34,30 +70,36 @@ export function AnimalInfoPage(props: StackHeaderProps) {
             </BackButton>
           </BackButtonArea>
           <FarmIconBox>
-            <FarmIconIMage source={PoultryImage} />
+            <FarmIconIMage
+              style={headerIconImage[data.tipoAnimal].style}
+              source={headerIconImage[data.tipoAnimal].url}
+            />
           </FarmIconBox>
         </HeaderContent>
       </HeaderRoot>
       <ContentScreen>
         <FieldPropertyInfo
           propertyName="Tipo de animal"
-          propertyValue="Suíno"
+          propertyValue={headerIconImage[data.tipoAnimal].text}
         />
-        <FieldPropertyInfo propertyName="Nome" propertyValue="Pepa Pig" />
+        <FieldPropertyInfo propertyName="Nome" propertyValue={data.nome} />
         <FieldPropertyInfo propertyName="Localização" propertyValue="Sala 1" />
         <FieldPropertyInfo
           propertyName="Data de nascimento"
-          propertyValue="20/06/2020"
+          propertyValue={data.dataNascimento}
         />
         <FieldPropertyInfo
           propertyName="Entrada no plantel"
-          propertyValue="20/06/2020"
+          propertyValue={data.entradaPlantel}
         />
-        <FieldPropertyInfo propertyName="Peso na compra" propertyValue="64" />
+        <FieldPropertyInfo
+          propertyName="Peso na compra"
+          propertyValue={data.pesoCompra}
+        />
         <FieldPropertyInfo propertyName="Raça" propertyValue="Porco Rosa" />
         <FieldPropertyInfo
           propertyName="Código de rastreamento"
-          propertyValue="2s9s1a5q56556521d5a"
+          propertyValue={data.codigoRastreamento}
         />
         <FieldPropertyInfo
           propertyName="Fase de produção"
@@ -65,7 +107,7 @@ export function AnimalInfoPage(props: StackHeaderProps) {
         />
         <FieldPropertyInfo
           propertyName="Tipo de granja"
-          propertyValue="Recria"
+          propertyValue={data.tipoGranja}
         />
         <FieldPropertyInfo propertyName="Status" propertyValue="Vendido" />
       </ContentScreen>
